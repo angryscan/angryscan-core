fbb4241b3a305ab272076a2ba8e288cfe5ef5fda
StellaLupus <soulofpain.k@gmail.com>
2025-03-27 16:08:58 +0300

    Bump version to 1.1.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 val dummyAttribute = Attribute.of("ru.packetdima", String::class.java)
 
 group = "ru.packetdima"
-version = System.getenv("VERSION") ?: "1.1.3"
+version = System.getenv("VERSION") ?: "1.1.4"
 
 subprojects {
     group = rootProject.group
