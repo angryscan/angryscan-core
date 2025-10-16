e68dc8692e9a92178b7c1cf0cc26e9388277c767
artemtarasov <tematarasov48rus@mail.ru>
2025-10-16 14:56:13 +0300

    [feature] Adding r1 detectors

--- kotlin-lib/src/commonMain/kotlin/org/angryscan/common/matchers/StateRegContract.kt
+++ kotlin-lib/src/commonMain/kotlin/org/angryscan/common/matchers/StateRegContract.kt
@@ -4,12 +4,10 @@ import info.downdetector.bigdatascanner.common.extensions.regexDetector
 
 private val STATE_REG_CONTRACT_FL_REGEX_ONLY = """
 (?ix)
-(?<!\S)
-(?:номер\s+государственной\s+регистрации\s+договора|номер\s+регистрации\s+договора\s+в\s+Росреестре|госрегистрационный\s+номер\s+договора\s+с\s+ФЛ|запись\s+регистрации)?
+(?<![\p{L}\d\p{S}\p{P}])
+(?:номер\s+государственной\s+регистрации\s+договора|номер\s+регистрации\s+договора\s+в\s+Росреестре|госрегистрационный\s+номер\s+договора\s+с\s+ФЛ)?
 \s*[:\-]?\s*
-(?:№\s?|N\s?)?
 (\d{2}\s?[-:]\s?\d{2}\s?[-:]\s?\d{2}\s?/\s?\d{3,4}\s?/\s?\d{4}\s?[-:]\s?\d{1,3})
-(?! \S)
 """.trimIndent()
 
 fun findStateRegContract(text: String, withContext: Boolean) = regexDetector(
