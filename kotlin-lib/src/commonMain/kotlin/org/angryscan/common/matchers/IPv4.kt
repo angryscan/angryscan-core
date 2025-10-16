866649363ebf1d56236067c2edce5444e1b801f8
StellaLupus <soulofpain.k@gmail.com>
2025-10-17 00:01:24 +0300

    [fix] IPv4 FP matching

--- kotlin-lib/src/commonMain/kotlin/org/angryscan/common/matchers/IPv4.kt
+++ kotlin-lib/src/commonMain/kotlin/org/angryscan/common/matchers/IPv4.kt
@@ -9,14 +9,14 @@ import org.angryscan.common.engine.kotlin.IKotlinMatcher
 object IPv4 : IHyperMatcher, IKotlinMatcher {
     override val name = "IPv4"
     override val javaPatterns = listOf(
-        """(?:[^0-9.])((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(?:[^0-9.])"""
+        """(?:[^0-9.:])((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(?:[^0-9.])"""
     )
     override val regexOptions = setOf(
         RegexOption.MULTILINE
     )
 
     override val hyperPatterns = listOf(
-        """(?:[^0-9.])((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(?:[^0-9.])"""
+        """(?:[^0-9.:])((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.){3}(25[0-5]|(2[0-4]|1\d|[1-9]|)\d)(?:[^0-9.])"""
     )
     override val expressionOptions = setOf(
         ExpressionOption.MULTILINE,
