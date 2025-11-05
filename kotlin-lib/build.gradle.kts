cabb440464f016b089295db4bebace71ab52afb3
StellaLupus <soulofpain.k@gmail.com>
2025-11-05 15:12:09 +0300

    [feature] Remove hyperscan from building jar Bump version to 1.3.6

--- kotlin-lib/build.gradle.kts
+++ kotlin-lib/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 }
 
 group = "org.angryscan"
-version = "1.3.5"
+version = "1.3.6"
 description = "Data Scanner Library"
 
 
@@ -78,7 +78,7 @@ kotlin {
         }
         jvmMain {
             dependencies {
-                implementation(libs.hyperscan)
+                compileOnly(libs.hyperscan)
             }
         }
     }
