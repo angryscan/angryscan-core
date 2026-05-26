5f5527d36e4b43b59218137346d74436c3a5a9cf
artemtarasov <tematarasov48rus@mail.ru>
2026-05-26 14:58:11 +0300

    [fix] Deleted comments

--- kotlin-lib/src/commonMain/kotlin/org/angryscan/common/matchers/Birthday.kt
+++ kotlin-lib/src/commonMain/kotlin/org/angryscan/common/matchers/Birthday.kt
@@ -7,7 +7,7 @@ import org.angryscan.common.engine.kotlin.IKotlinMatcher
 
 /**
  * Matcher for birth dates in Russian and English text.
- * Russian: preceded by "дата рождения", "родился", "родилась" + DD.MM.YYYY or DD Month YYYY.
+ * Russian: preceded by birth-date keywords + DD.MM.YYYY or DD Month YYYY.
  * English: preceded by "date of birth", "dob", "birth date", "born" + MM/DD/YYYY or DD/MM/YYYY.
  */
 @Serializable
@@ -18,9 +18,9 @@ object Birthday : IHyperMatcher, IKotlinMatcher {
     private val enDatePattern = """(?:0?[1-9]|1[0-2])[/\-\.](?:0?[1-9]|[12]\d|3[01])[/\-\.](?:19|20)\d{2}"""
 
     override val javaPatterns = listOf(
-        // Russian: дата рождения + DD.MM.YYYY
+        // Russian keywords + DD.MM.YYYY
         """(?<![\p{L}\d])(?:дата\s+рожд[а-яёА-ЯЁ]+|родил(?:ся(?:\(лась\))?|ась))\s*[:\-]?\s*$ruDatePattern(?![\p{L}\d])""",
-        // Russian: дата рождения + DD Month YYYY
+        // Russian keywords + DD Month YYYY
         """(?<![\p{L}\d])(?:дата\s+рожд[а-яёА-ЯЁ]+|родил(?:ся(?:\(лась\))?|ась))\s*[:\-]?\s*(?:0?[1-9]|[12]\d|3[01])\s+(?:янв(?:арь|аря)?|фев(?:раль|раля)?|мар(?:т|та)?|апр(?:ель|еля)?|май|мая|июн(?:ь|я)?|июл(?:ь|я)?|авг(?:уст|уста)?|сен(?:тябрь|тября)?|сент\.?|окт(?:ябрь|ября)?|ноя(?:брь|бря)?|дек(?:ябрь|бря)?)\s+((?:19|20)\d{2}|\d{2})(?![\p{L}\d])""",
         // English: date of birth / dob / birth date / born + MM/DD/YYYY
         """(?i)(?<![\p{L}\d])(?:date\s+of\s+birth|d\.?o\.?b\.?|birth\s*date|born(?:\s+on)?)\s*[:\-]?\s*$enDatePattern(?![\p{L}\d])"""
@@ -31,9 +31,9 @@ object Birthday : IHyperMatcher, IKotlinMatcher {
     )
 
     override val hyperPatterns: List<String> = listOf(
-        // Russian: дата рождения + DD.MM.YYYY
+        // Russian keywords + DD.MM.YYYY
         """(?:^|[^a-zA-Z0-9А-ЯЁа-яё])(?:дата\s+рожд[а-яёА-ЯЁ]+|родил(?:ся(?:\(лась\))?|ась))\s*[:\-]?\s*$ruDatePattern(?:[^0-9]|$)""",
-        // Russian: дата рождения + DD Month YYYY
+        // Russian keywords + DD Month YYYY
         """(?:^|[^a-zA-Z0-9А-ЯЁа-яё])(?:дата\s+рожд[а-яёА-ЯЁ]+|родил(?:ся(?:\(лась\))?|ась))\s*[:\-]?\s*(?:0?[1-9]|[12]\d|3[01])\s+(?:янв(?:арь|аря)?|фев(?:раль|раля)?|мар(?:т|та)?|апр(?:ель|еля)?|май|мая|июн(?:ь|я)?|июл(?:ь|я)?|авг(?:уст|уста)?|сен(?:тябрь|тября)?|сент\.?|окт(?:ябрь|ября)?|ноя(?:брь|бря)?|дек(?:ябрь|бря)?)\s+((?:19|20)\d{2}|\d{2})(?:[^0-9]|$)""",
         // English: date of birth / dob / birth date / born + MM/DD/YYYY
         """(?i)(?:^|[^a-zA-Z0-9])(?:date\s+of\s+birth|d\.?o\.?b\.?|birth\s*date|born(?:\s+on)?)\s*[:\-]?\s*$enDatePattern(?:[^0-9]|$)"""
